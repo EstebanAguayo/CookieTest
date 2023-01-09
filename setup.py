cc68afcd2871008450124ab748be2b92eea9ca03
EstebanAguayo <s23162@dtu.dk>
2023-01-09 15:44:40 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='CookieModule',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a test for the first package strcucture based in CookieCutter V2',
+    author='Esteban',
+    license='',
+)
